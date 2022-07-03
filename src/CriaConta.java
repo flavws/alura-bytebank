54a82127f72d3c375048ba6eaba2e157720e5792
flavws <persona.fmaria@gmail.com>
2022-07-02 23:08:16 -0300

    segunda instância

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -9,5 +9,18 @@ public class CriaConta {
 		primeiraConta.saldo = 200;
 		
 		System.out.println(primeiraConta.saldo);
+		
+		//podemos usar operações também
+		primeiraConta.saldo += 100;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println("primeira conta tem: " + primeiraConta.saldo);
+		System.out.println("segunda conta tem: " + segundaConta.saldo);
+		
+		
 	}
 }
